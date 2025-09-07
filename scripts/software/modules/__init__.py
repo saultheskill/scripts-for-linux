8ab5fcbd7590c7c5bc2a5f02779ccdfe090b9fdb
root <root@ubuntu24.04>
2025-09-07 18:24:13 +0800

    refactor to static template

--- scripts/software/modules/__init__.py
+++ scripts/software/modules/__init__.py
@@ -2,15 +2,11 @@
 
 """
 Shell工具配置生成器模块包
-提供模块化的配置生成功能
+基于模板的配置生成系统
 """
 
 from .tool_detector import get_tool_detector, ToolDetector
 from .file_manager import get_file_manager, FileManager
-from .module_generators import ModuleGenerators
-from .advanced_modules import AdvancedModuleGenerators
-from .extended_modules import ExtendedModuleGenerators
-from .config_coordinator import get_config_coordinator, ConfigCoordinator
 from .template_coordinator import get_template_coordinator, TemplateCoordinator
 
 __all__ = [
@@ -18,15 +14,10 @@ __all__ = [
     'ToolDetector',
     'get_file_manager',
     'FileManager',
-    'ModuleGenerators',
-    'AdvancedModuleGenerators',
-    'ExtendedModuleGenerators',
-    'get_config_coordinator',
-    'ConfigCoordinator',
     'get_template_coordinator',
     'TemplateCoordinator'
 ]
 
 __version__ = "2.1.0"
 __author__ = "saul"
-__description__ = "模块化Shell工具配置生成器"
+__description__ = "基于模板的Shell工具配置生成器"
